492f4cae5d7e19650ae319107bb028f094ec5224
Joe Xue <lgxue@hotmail.com>
2026-09-10 21:02:39 -0400

    Add the popup menu
    	modified:   terminal/terminal.go

--- terminal/terminal.go
+++ terminal/terminal.go
@@ -1,7 +1,7 @@
 package terminal
 
 import (
-    "fmt"
+    //"fmt"
     "fyne.io/fyne/v2"
     "fyne.io/fyne/v2/driver/desktop"
     "fyne.io/fyne/v2/widget"
@@ -22,30 +22,11 @@ func New() *Terminal {
 	t := &Terminal{}
 	t.ExtendBaseWidget(t)
 
-	menuItems := make([]*fyne.MenuItem, 2, 2)
-	hsplitItem := fyne.NewMenuItemWithIcon(lang.L("Horionzontal Split"), assert.HSplitIconRes, func() {
-        if t.OnSplit != nil {
-            t.OnSplit(1)
-        }
-    })
-	vsplitItem := fyne.NewMenuItemWithIcon(lang.L("Vertical Split"), assert.VSplitIconRes, func() {
-        if t.OnSplit != nil {
-            t.OnSplit(2)
-        }
-    })
-
-    menuItems = append(menuItems, hsplitItem)
-    menuItems = append(menuItems, vsplitItem)
-
-	driver := fyne.CurrentApp().Driver()
-	c := driver.CanvasForObject(t)
-	t.popUpMenu = widget.NewPopUpMenu(fyne.NewMenu("", menuItems...), c)
 	return t
 }
 
 func (t *Terminal) TappedSecondary(pe *fyne.PointEvent) {
 	if c := fyne.CurrentApp().Driver().CanvasForObject(t); c != nil {
-        menuItems := make([]*fyne.MenuItem, 2, 2)
         hsplitItem := fyne.NewMenuItemWithIcon(lang.L("Horionzontal Split"), assert.HSplitIconRes, func() {
             if t.OnSplit != nil {
                 t.OnSplit(1)
@@ -57,17 +38,10 @@ func (t *Terminal) TappedSecondary(pe *fyne.PointEvent) {
             }
         })
 
-        //menuItems = append(menuItems, hsplitItem)
-        //menuItems = append(menuItems, vsplitItem)
-        menuItems[0] = hsplitItem
-        menuItems[1] = vsplitItem
-
-        //driver := fyne.CurrentApp().Driver()
-	    //c := driver.CanvasForObject(t)
+        menuItems := []*fyne.MenuItem {hsplitItem, vsplitItem}
 	    popUpMenu := widget.NewPopUpMenu(fyne.NewMenu("", menuItems...), c)
 	    popUpMenu.ShowAtPosition(pe.Position)
 	}
-    fmt.Println("Tapped")
 }
 
 func (t *Terminal) MouseDown(ev *desktop.MouseEvent) {
